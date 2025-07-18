081a7e6017aa839f384a16a1e68e2db47c5ff487
George <georgekutsia@gmail.com>
2025-07-18 18:14:06 +0200

    quitando imágenes y aligerando peso. por ahora imágenes grandes

--- assets/js/bushes.js
+++ assets/js/bushes.js
@@ -100,28 +100,28 @@ class Portal {
         return colX && colY;
     }
   }
-class Wall {
-    constructor(ctx, x, y, w, h , stn) {
-      this.ctx = ctx;
-      this.x = x;
-      this.y = y;
-      this.w = w;
-      this.h = h;
-      this.stn = stn
-      this.img = new Image();
-      this.img.src = "/assets/images/fondos/brick-png-39828.png";
-    }
-    draw() {
-      this.ctx.drawImage(
-        this.img, this.x, this.y, this.w++, this.h
-      )
-    } 
-    collides(player) {
-      const colX = this.x <= player.x + player.w && this.x + this.w > player.x;
-      const colY = this.y + this.h > player.y && this.y < player.y + player.h;
-      return colX && colY;
-    }
-  }
+// class Wall { quitar
+//     constructor(ctx, x, y, w, h , stn) {
+//       this.ctx = ctx;
+//       this.x = x;
+//       this.y = y;
+//       this.w = w;
+//       this.h = h;
+//       this.stn = stn
+//       this.img = new Image();
+//       this.img.src = "/assets/images/fondos/brick-png-39828.png";
+//     }
+//     draw() {
+//       this.ctx.drawImage(
+//         this.img, this.x, this.y, this.w++, this.h
+//       )
+//     } 
+//     collides(player) {
+//       const colX = this.x <= player.x + player.w && this.x + this.w > player.x;
+//       const colY = this.y + this.h > player.y && this.y < player.y + player.h;
+//       return colX && colY;
+//     }
+//   }
 class Cactus {
     constructor(ctx, x, y, w, h , cactusImg) {
       this.ctx = ctx;
